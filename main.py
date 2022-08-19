54b7155dea85676c27831f13af71653d17d19853
lala7nagita <111584393+lala7nagita@users.noreply.github.com>
2022-08-19 10:54:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 path = "C:/Users/Kevin/Desktop/PyBot/live_data.json"
 
+#randomizes numbers for sleep timer
 def rng(first,second):
     options = [0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4]
     dec = [0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09]
@@ -16,11 +17,13 @@ while True:
     myjson = open(path,"r")
     jsondata = myjson.read()
 
+    #just here to solve an error I was getting
     try:
         objt = json.loads(jsondata)
     except json.decoder.JSONDecodeError:
         print("f")
 
+    #checks inventory and assigns variables
     inv = objt['inventory']
     equip = objt['equipment']
     skills = objt['skills']
@@ -29,6 +32,7 @@ while True:
     neck = equip[2].get("index")
     hp = skills[5].get("boostedLevel")
     
+    #runs the statements to open/close backpack with f2 and f4
     if hp <= 10 or neck !=2:
         keyboard.press(Key.f2)
         time.sleep(rng(6,9))
